08f2ec39c695b06319b391db68df893ca5636c38
emilyalbert <FunkCrusade@outlook.com>
2024-01-18 18:27:35 -0600

    project setup

---
+++ liftoff-jamey-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace liftoff_jamey_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
