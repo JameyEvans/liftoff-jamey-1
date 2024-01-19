29169e30048458d2d2d8a3bf602eba840ad97985
Liz <adammvehige@gmail.com>
2024-01-19 16:57:50 -0600

    started work on the search function

---
+++ BookWorm/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace BookWorm.Controllers
+{
+    public class SearchController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index() 
+        { 
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Index(string searchTerm)
+        {
+            return View();
+        }
+    }
+}
