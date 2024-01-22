d1b7812b7a81339045cf7dc887335bce1dc6cd05
Liz <adammvehige@gmail.com>
2024-01-22 17:47:09 -0600

    starting work on api calls

---
+++ liftoff-jamey-1/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using liftoff_jamey_1.Models;
+
+namespace liftoff_jamey_1.Controllers
+
+{
+    public class SearchContoller : Controller
+    {
+
+    }
+}
---
+++ liftoff-jamey-1/wwwroot/js/openLibraryCalls.js
@@ -0,0 +1,20 @@
+﻿
+
+document.addEventListener("DOMContentLoaded", () => {
+    const searchResults = document.getElementById("searchResults");
+
+    async function getSearchResults(searchTerm) {
+        const fetchResult = await fetch(`https://openlibrary.org/search.json?q=${searchTerm}`);
+
+        if (!fetchResult.ok) {
+            return "Failed to fetch search from api";
+        }
+        const jsonData = await fetchResult.json();
+        clientSearch.innerHTML = jsonData[0].title
+    }
+
+    getSearchResults();
+
+
+
+})
