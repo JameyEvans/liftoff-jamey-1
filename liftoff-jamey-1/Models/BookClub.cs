e1324bd3a7f75f6fa30611e03c478cc773e811ea
karengp0207 <karengp0207@gmail.com>
2024-01-22 21:51:37 -0600

    Created the BookClub.cs in Models folder

---
+++ liftoff-jamey-1/Models/BookClub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace liftoff_jamey_1.Models
+{
+    public class BookClub
+    {
+        public int? Id { get; set; }
+        public string? ClubName { get; set; }
+        public string? Location { get; set; }
+        public string? PublicOrPrivate { get; set; } //not sure about this one.
+        //public bool IsPublic { get; set;}
+
+        public BookClub(string clubName)
+        {
+            ClubName = clubName;
+        }
+
+        public BookClub() { }
+    }
+}
