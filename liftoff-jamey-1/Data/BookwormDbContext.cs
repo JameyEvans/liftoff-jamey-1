d9429c403cbe3b95729e0f9d4e282ed95c0792e2
Jada <jada.hundley@health.slu.edu>
2024-01-22 21:06:57 -0600

    added dbcontext class and model class

---
+++ liftoff-jamey-1/Data/BookwormDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace liftoff_jamey_1.Data
+{
+	public class BookwormDbContext : DbContext
+	{
+		//DbSet<UserLogIn> UserLogIns { get; set; }
+		public BookwormDbContext(DbContextOptions<BookwormDbContext> options) : base(options)
+		{
+		}
+	}
+}
+
--- liftoff-jamey-1/Program.cs
+++ liftoff-jamey-1/Program.cs
@@ -11,14 +11,16 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        var connectionString = "server=localhost;user=bookworm;password=thelaunchclub;database=bookworm";
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
+        /*var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlite(connectionString));
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();*/
 
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-        builder.Services.AddControllersWithViews();
+        //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        //    .AddEntityFrameworkStores<ApplicationDbContext>();
+        //builder.Services.AddControllersWithViews();
 
         var app = builder.Build();
 
