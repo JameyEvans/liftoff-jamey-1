5c96f519bd122113b47ecc0bff779fd602ba35b4
Liz <adammvehige@gmail.com>
2024-01-24 18:42:02 -0600

    fixed string conversion for search, still struggling with json file

--- liftoff-jamey-1/wwwroot/js/openLibraryCalls.js
+++ liftoff-jamey-1/wwwroot/js/openLibraryCalls.js
@@ -6,14 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("submit").addEventListener("click", () => {
         const searchResults = document.getElementById("searchResults");
 
+        
         async function getSearchResults(searchTerm) {
-            const fetchResult = await fetch(`https://openlibrary.org/search.json?q=${searchTerm.Replace(" ", "+")}`);
+
+            const fetchResult = await fetch(`https://openlibrary.org/search.json?q=${searchTerm.toString().replace(" ", '+')}`);
 
             if (!fetchResult.ok) {
                 return "Failed to fetch search from api";
             }
             const jsonData = await fetchResult.json();
-            clientSearch.innerHTML = jsonData[0].title
+            searchResults.innerHTML = jsonData[0].title;
         }
 
         getSearchResults(document.getElementById("searchQuery"));
