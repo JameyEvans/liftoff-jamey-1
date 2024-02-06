4c7d19f951267de5f3934abb27e5732485c1773b
karengp0207 <karengp0207@gmail.com>
2024-02-05 19:55:53 -0600

    cleaned up mvcs

--- liftoff-jamey-1/Models/UpdateBookClubViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace liftoff_jamey_1.Models
-{
-	public class UpdateBookClubViewModel
-	{
-		public int? Id { get; set; }
-		public string? ClubName { get; set; }
-		public string? Location { get; set; }
-	}
-}
