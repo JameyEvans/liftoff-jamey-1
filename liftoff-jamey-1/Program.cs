895213429d0f85b33ab919c6246a639a90c05ced
Jamey Evans <jamey270@gmail.com>
2024-02-12 17:11:12 -0600

    Updated appsettings.json and program.cs to prevent you from having to type about the entire filepath to the database.

--- liftoff-jamey-1/Program.cs
+++ liftoff-jamey-1/Program.cs
@@ -20,17 +20,36 @@ IConfiguration configuration = new ConfigurationBuilder()
 			.AddJsonFile("appsettings.json")
 			.Build();
 
+// Set SQlite connection string
+
+bool useAbsolutePath = builder.Configuration.GetValue<bool>("Sqlite:UseAbsolutePath", false);
+string sqliteConnectionString;
+
+if (useAbsolutePath)
+{
+    string dbPath = builder.Configuration.GetValue<string>("Sqlite:DbPath");
+    sqliteConnectionString = $"Data Source={dbPath}";
+}
+else
+{
+    // get directory to project before building
+    string projectPath = AppDomain.CurrentDomain.BaseDirectory.Split(new String[] { @"bin" }, StringSplitOptions.None)[0];
+    string dbPath = builder.Configuration.GetValue<string>("Sqlite:DbPath");
+    dbPath = System.IO.Path.Combine(projectPath, dbPath);
+    sqliteConnectionString = $"Data Source={dbPath}";
+}
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlite(sqliteConnectionString));
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
-    {
-	    app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
+{
+	app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
