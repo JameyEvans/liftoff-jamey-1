9abba885cc7ad413d13834fa2e9c0d49e35a11e5
Liz <adammvehige@gmail.com>
2024-02-12 17:44:03 -0600

    search functioning, needs more work to make it more readable

--- liftoff-jamey-1/wwwroot/js/openLibraryCalls.js
+++ liftoff-jamey-1/wwwroot/js/openLibraryCalls.js
@@ -4,16 +4,19 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const searchResults = document.getElementById("searchResults");
+    const searchResults = document.getElementById("searchResult");
 
-    document.getElementById("submit").addEventListener("click", () => {
-        
+    document.getElementById("submit").addEventListener("click", (event) => {
+
+        event.preventDefault();
         const searchQuery = document.getElementById("searchQuery").value;
 
         console.log(searchQuery)
 
         async function getSearchResults(searchTerm) {
-        
+
+            searchResults.innerHTML = ''
+            
             const fetchResult = await fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(searchTerm)}`);
 
             console.log(fetchResult);
@@ -22,8 +25,17 @@ document.addEventListener("DOMContentLoaded", () => {
                 return "Failed to fetch search from api";
             }
             const jsonData = await fetchResult.json();
-            
-            searchResults.innerHTML = jsonData;   
+
+            const doc = jsonData.docs;
+            console.log(doc[0].title)
+
+           
+
+            for (i = 0; i < 20; i++) {
+                searchResult.innerHTML += `<tr>${i + 1}</tr><tr>${doc[i].title}</tr><tr>${doc[i].author_name}</tr><tr>${doc[i].publish_year}</tr>`
+            }
+
+            searchResults.innerHTML = (doc) ? doc : "No results found."
         }
         
 
