3be939a30eba143dea35c31c8ecfc3bff0573a5e
emilyalbert <FunkCrusade@outlook.com>
2024-02-15 15:05:38 -0600

    changed bookclub to a list

--- liftoff-jamey-1/Controllers/BookClubController.cs
+++ liftoff-jamey-1/Controllers/BookClubController.cs
@@ -8,17 +8,17 @@ namespace liftoff_jamey_1.Controllers
 {
 	public class BookClubController : Controller
 	{
-		private readonly ApplicationDbContext _db;
-		public BookClubController(ApplicationDbContext db)
+		private readonly ApplicationDbContext _context;
+		public BookClubController(ApplicationDbContext context)
 		{
-			_db = db;
+			_context = context;
 		}
 
 
 		public IActionResult Index()
 		{
-			IEnumerable<BookClub> objBookClubList = _db.BookClubs;
-			return View(objBookClubList);
+            List<BookClub> bookClubs = _context.BookClubs.ToList();
+            return View(bookClubs);
 		}
 
         //GET
@@ -33,8 +33,8 @@ namespace liftoff_jamey_1.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(BookClub obj)
         {
-            _db.BookClubs.Add(obj);
-            _db.SaveChanges();
+            _context.BookClubs.Add(obj);
+            _context.SaveChanges();
             return RedirectToAction("Index");
         }
     }
