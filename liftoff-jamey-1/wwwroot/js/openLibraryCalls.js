86775cb86bd22efcc0b4676e394f6d8c9e8c1408
karengp0207 <karengp0207@gmail.com>
2024-02-17 20:03:43 -0600

    Still working on getting "no cover image found" to show up in the search table.

--- liftoff-jamey-1/wwwroot/js/openLibraryCalls.js
+++ liftoff-jamey-1/wwwroot/js/openLibraryCalls.js
@@ -26,15 +26,31 @@ document.addEventListener("DOMContentLoaded", () => {
             }
             const jsonData = await fetchResult.json();
 
-            for (i = 1; i < 21; i++) {
-                let coverUrl = `https://covers.openlibrary.org/b/oclc/${jsonData.docs[i].oclc}.jpg`;
-                searchResults.innerHTML += `<tr><td>${[i]}</td><td>${jsonData.docs[i].title}</td><td>${jsonData.docs[i].author_name}</td><td>${jsonData.docs[i].first_publish_year}</td><td><img class="book-cover" src="${coverUrl}"></td></tr>`;
+            for (let i = 0; i < 20; i++) {
+                let coverUrl = `https://covers.openlibrary.org/b/lccn/${jsonData.docs[i].lccn}-S.jpg`;
+                let coverImageHTML = jsonData.docs[i].cover_i ? `<img class="book-cover" src="${coverUrl}" alt="Book Cover">` : "<span>No Image Found</span>";
+                searchResults.innerHTML += `<tr><td>${i+1}</td><td>${jsonData.docs[i].title}</td><td>${jsonData.docs[i].author_name}</td><td>${jsonData.docs[i].first_publish_year}</td><td>${coverImageHTML}</td></tr>`;
+
             }
-            
-                searchResults.innerHTML = (doc) ? doc : "No results found.";
+            if (jsonData.docs.length === 0) {
+                searchResults.innerHTML = "No results found.";
+            }
+
+
+            //searchResults.innerHTML = (doc) ? doc : "No results found.";
         }
 
 
         getSearchResults(searchQuery);
     })
 })
+
+
+
+
+/*for (let i = 0; i < 20; i++) {
+    let coverUrl = `https://covers.openlibrary.org/b/lccn/${jsonData.docs[i].lccn}-S.jpg`;
+    let coverImageHTML = jsonData.docs[i].cover_i ? `<img class="book-cover" src="${coverUrl}" alt="Book Cover">` : "<span>No Image Found</span>";
+    searchResults.innerHTML += `<tr><td>${i + 1}</td><td>${jsonData.docs[i].title}</td><td>${jsonData.docs[i].author_name}</td><td>${jsonData.docs[i].first_publish_year}</td><td>${coverImageHTML}</td></tr>`;
+
+}*/
